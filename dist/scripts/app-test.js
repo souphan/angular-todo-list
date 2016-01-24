dfdc63bb20b647629562f3d4615a743641251374
Souphan <souphan.ctr@gmail.com>
2016-01-23 21:47:35 -0600

    verified that I can add entries to firebase

--- dist/scripts/app-test.js
+++ dist/scripts/app-test.js
@@ -1,28 +1,65 @@
-(function() {
-    function config($stateProvider, $locationProvider) {
-        $locationProvider
-         .html5Mode({
-             enabled: true,
-             requireBase: false
-         });
-        $stateProvider
-         .state('landing', {
-             url: '/',
-             controller: 'LandingCtrl as landing',
-             templateUrl: '/templates/landing.html'
-         })
-        .state('album', {
-             url: '/album',
-             templateUrl: '/templates/album.html'
-         })
-        .state('collection', {
-             url: '/collection',
-             controller: 'CollectionCtrl as collection',
-             templateUrl: '/templates/collection.html'
-        });
-    }
-     
-angular
-    .module('blocJams', ['ui.router'])
-    .config(config);
- })();
+
+keodoToDo.config(function($stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/state1");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('state1', {
+      url: "/state1",
+      templateUrl: "partials/state1.html"
+    })
+    .state('state1.list', {
+      url: "/list",
+      templateUrl: "partials/state1.list.html",
+      controller: function($scope) {
+        $scope.items = ["A", "List", "Of", "Items"];
+      }
+    })
+    .state('state2', {
+      url: "/state2",
+      templateUrl: "partials/state2.html"
+    })
+    .state('state2.list', {
+      url: "/list",
+      templateUrl: "partials/state2.list.html",
+      controller: function($scope) {
+        $scope.things = ["A", "Set", "Of", "Things"];
+      }
+    });
+});
+
+var appRouter = angular.module("keodoToDo", ["ui.router"]);
+
+var appAngular = angular.module("keodoToDo", ["firebase"]);
+
+app.controller("KeodoToDoCtrl", ["$scope", $firebaseArray",
+  function($scope, $firebaseArray) {
+    var messagesRef = new Firebase("https://keodo-todo-list.firebaseio.com/");
+
+  // download the data into a local object
+  var syncObject = $firebaseObject(ref);
+
+  // download the data from a Firebase reference into a (pseudo read-only) array
+  // all server changes are applied in realtime
+  $scope.messages = $firebaseArray(messagesRef);
+                                 
+  // create a query for the most recent 25 messages on the server
+  var query = messagesRef.orderByChild("timestamp").limitToLast(25);
+
+  // the $firebaseArray service properly handles database queries as well
+  $scope.filteredMessages = $firebaseArray(query);
+
+  // add new items to the array
+  // the message is automatically added to our Firebase database!
+  $scope.addMessage = function() {
+    $scope.messages.$add({
+      text: $scope.newMessageText
+    });
+  };
+
+  // synchronize the object with a three-way data binding
+  // click on `index.html` above to see it used in the DOM!
+  syncObject.$bindTo($scope, "data");
+});
--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,45 +1,30 @@
-keodoToDo.config(function($stateProvider, $urlRouterProvider) {
-  //
-  // For any unmatched url, redirect to /state1
-  $urlRouterProvider.otherwise("/state1");
-  //
-  // Now set up the states
-  $stateProvider
-    .state('state1', {
-      url: "/state1",
-      templateUrl: "partials/state1.html"
-    })
-    .state('state1.list', {
-      url: "/list",
-      templateUrl: "partials/state1.list.html",
-      controller: function($scope) {
-        $scope.items = ["A", "List", "Of", "Items"];
-      }
-    })
-    .state('state2', {
-      url: "/state2",
-      templateUrl: "partials/state2.html"
-    })
-    .state('state2.list', {
-      url: "/list",
-      templateUrl: "partials/state2.list.html",
-      controller: function($scope) {
-        $scope.things = ["A", "Set", "Of", "Things"];
-      }
-    });
-});
+var myApp = angular.module("keodoToDo", ["ui.router", "firebase"]);
 
-var appRouter = angular.module("keodoToDo", ["ui.router"]);
-
-var appAngular = angular.module("keodoToDo", ["firebase"]);
-
-app.controller("KeodoToDoCtrl", function($scope, $firebaseObject) {
+myApp.controller("MyController", ["$scope", "$firebaseArray",
+function($scope, $firebaseArray) {
+  //CREATE A FIREBASE REFERENCE
   var ref = new Firebase("https://keodo-todo-list.firebaseio.com/");
 
-  // download the data into a local object
-  var syncObject = $firebaseObject(ref);
+  // GET MESSAGES AS AN ARRAY
+  $scope.messages = $firebaseArray(ref);
 
-  // synchronize the object with a three-way data binding
-  // click on `index.html` above to see it used in the DOM!
-  syncObject.$bindTo($scope, "data");
-});
+  //ADD MESSAGE METHOD
+  $scope.addMessage = function(e) {
+
+    //LISTEN FOR RETURN KEY
+    if (e.keyCode === 13 && $scope.msg) {
+      //ALLOW CUSTOM OR ANONYMOUS USER NAMES
+      var name = $scope.name || "anonymous";
+
+      //ADD TO FIREBASE
+      $scope.messages.$add({
+        from: name,
+        body: $scope.msg
+      });
+
+      //RESET MESSAGE
+      $scope.msg = "";
+    }
+  }
+}
+]);
